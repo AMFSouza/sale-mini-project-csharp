89c3b550f179965a90b9f6030ef20c3095853b5f
AMFSouza <amfdeveloper@gmail.com>
2021-10-08 17:20:33 -0300

    OrderStatus enumeration

---
+++ src/Sale-Mini-Project/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sale_Mini_Project.Entities.Enums
+{
+    enum OrderStatus
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+
+    }
+}
