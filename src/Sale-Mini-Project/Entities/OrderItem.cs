20d09c110137f63c1e901dc0ee8bc24df177273c
AMFSouza <amfdeveloper@gmail.com>
2021-10-09 02:44:48 -0300

    class OrdemItem created

---
+++ src/Sale-Mini-Project/Entities/OrderItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Sale_Mini_Project.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public OrderItem() { }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public Double Subtotal()
+        {
+            return Quantity * Price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Quantity}, ${Subtotal().ToString("F2", CultureInfo.InvariantCulture)} ";
+        }
+    }
+}
