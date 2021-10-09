998df8e49a0e57cb1cf689b583817486ee4105ce
AMFSouza <amfdeveloper@gmail.com>
2021-10-09 02:35:27 -0300

    Override ToString() on class Product

--- src/Sale-Mini-Project/Entities/Product.cs
+++ src/Sale-Mini-Project/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Sale_Mini_Project.Entities
@@ -9,15 +10,17 @@ namespace Sale_Mini_Project.Entities
         public string Name { get; set; }
         public double Price { get; set; }
 
-        public Product()
-        {
-
-        }
+        public Product() { }
 
         public Product(string name, double price)
         {
             Name = name;
             Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
