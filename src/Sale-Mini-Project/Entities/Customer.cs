b70d7a90ad31ed341e46825aa37c87efab8a5178
AMFSouza <amfdeveloper@gmail.com>
2021-10-09 01:18:30 -0300

    class Customer

---
+++ src/Sale-Mini-Project/Entities/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sale_Mini_Project.Entities
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Customer() { }
+
+        public Customer(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + "(" + BirthDate.ToString() + ")" + " - " + Email; 
+        }
+    }
+}
