42cb43c0b943b854afbeeb4adf9c1effbd64a085
AMFSouza <amfdeveloper@gmail.com>
2021-10-14 00:33:26 -0300

    Program.cs

--- src/Sale-Mini-Project/Program.cs
+++ src/Sale-Mini-Project/Program.cs
@@ -10,8 +10,28 @@ namespace Sale_Mini_Project
     {
         static void Main(string[] args)
         {
+            MessageOnTop();
+            Customer customer = GetCustomer();
+            Order order = GetOrder(customer);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            ProcessOrder(n, order);
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+        }
+
+        static void MessageOnTop()
+        {
             Console.WriteLine("Enter the customer data:");
-            
+
+        }
+
+        static Customer GetCustomer()
+        {
             // customer
             Console.Write("Name: ");
             string name = Console.ReadLine();
@@ -20,15 +40,20 @@ namespace Sale_Mini_Project
             Console.Write("Birthdate: (DD/MM/YYYY): ");
             DateTime birthdate = DateTime.Parse(Console.ReadLine());
             Customer customer = new Customer(name, email, birthdate);
+            return customer;
+        }
 
-            // order data
+        static Order GetOrder(Customer customer)
+        {
             Console.WriteLine("Enter order data:");
             Console.Write("Status: ");
             OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
             Order order = new Order(DateTime.Now, status, customer);
-            Console.Write("How many items to this order? ");
-            int n = int.Parse(Console.ReadLine());
+            return order;
+        }
 
+        static void ProcessOrder(int n, Order order)
+        {
             for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine($"Enter #{i} item data:");
@@ -44,11 +69,8 @@ namespace Sale_Mini_Project
                 Product product = new Product(productName, price);
                 order.AddItem(new OrderItem(quantity, price, product));
             }
-
-            Console.WriteLine();
-            Console.WriteLine("ORDER SUMMARY:");
-            Console.WriteLine(order);
-
         }
+
+
     }
 }
